8f1b7c4fe8397f8e996310110a2b456e2c18c637
hrk <heerko@swummoq.net>
2019-06-09 22:53:40 +0200

    minor script changes

--- generate.sh
+++ generate.sh
@@ -7,26 +7,27 @@ import sys, getopt
 import random
 import subprocess
 
+pathlib.Path( pathlib.Path.cwd() / 'srcdocs' / 'clean' ).mkdir( exist_ok=True )
 src_path = pathlib.Path.cwd() / 'srcdocs'
-pathlib.Path( src_path / 'clean' ).mkdir( exist_ok=True )
 dest_path = src_path / 'clean'
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hcb",["clean", "build"])
-   except getopt.GetoptError:
-      print ('generate.py -c')
-      sys.exit(2)
-   for opt, arg in opts:
-	   if opt == '-h':
-		   print ('generate.py -c/clean | -b/build')
-		   sys.exit()
-	   elif opt in ("-c", "--clean"):
-		   cleanFiles()
-	   elif opt in ("-b", "--build"):
-		   build()
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt( argv, "hcbo:", [ "clean", "build", "output" ] )
+    except getopt.GetoptError:
+        print ( 'generate.py -c/--clean | -b/--build' )
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ( 'generate.py -c/--clean | -b/--build' )
+            sys.exit()
+        elif opt in ( "-c", "--clean "):
+            cleanFiles()
+        elif opt in ( "-b", "--build" ):
+            build()
+
 
 def cleanFiles():
 	for file in sorted( src_path.rglob( '*.html' ) ):
@@ -34,21 +35,21 @@ def cleanFiles():
 			content = src_file.read()
 			soup = BeautifulSoup( content, 'html.parser' )
 
-			for style in soup.head.find_all('style'):
+			for style in soup.head.find_all( 'style' ):
 				style.decompose()
 
-			for class_el in soup.select('[class]'):
+			for class_el in soup.select( '[class]' ):
 				del class_el[ 'class' ]
 
-			for p_span in soup.select('p > span, h1 > span'):
+			for p_span in soup.select( 'p > span, h1 > span' ):
 				p_span.unwrap()
 
-			for empty_el in soup.select(':empty'):
+			for empty_el in soup.select( ':empty' ):
 				empty_el.decompose()
 
 			clean = soup.prettify()
 
-			with open( dest_path / file.name, 'w') as dest_file:
+			with open( dest_path / file.name, 'w' ) as dest_file:
 				dest_file.write( clean )
 
 def build():
@@ -71,18 +72,18 @@ def build():
 	pdf_path = pathlib.Path.cwd() / 'build' / 'book.pdf'
 	print( str( pdf_path ) )
 
-	subprocess.Popen(['weasyprint %s %s' % (dest_file, pdf_path)], shell = True, stdout=subprocess.DEVNULL)
+	subprocess.Popen( [ 'weasyprint %s %s' % ( dest_file, pdf_path ) ], shell = True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL )
 
 def formatDocument( content ):
 	soup = BeautifulSoup( content, 'html.parser' )
 	body = soup.body
 	body.name = 'article'
-	body['class'] = 'cols-' + str(random.randint(2, 4)) + ' bodyfont-' + str(random.randint(1, 8))
-	for header in body.select('h1,h2'):
-		header['class'] = "headingfont-" + str(random.randint(1, 18))
+	body[ 'class' ] = 'cols-' + str( random.randint( 2, 4 ) ) + ' bodyfont-' + str( random.randint( 1, 8 ) )
+	for header in body.select( 'h1, h2' ):
+		header[ 'class' ] = "headingfont-" + str( random.randint( 1, 18 ) )
 
 	return body
 
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   main( sys.argv[ 1: ] )
