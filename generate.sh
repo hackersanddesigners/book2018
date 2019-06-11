5fa7189a307a63627ab62d1ad2fa98d2cb7f62c4
hrk <heerko@swummoq.net>
2019-06-11 10:12:32 +0200

    H1 elements in an article are full width now. Location of /clean has moved to /build

--- generate.sh
+++ generate.sh
@@ -1,89 +1,95 @@
 #! /usr/bin/env python
 
-import os
+import os, sys, getopt, random, subprocess
 import pathlib
 from bs4 import BeautifulSoup
-import sys, getopt
-import random
-import subprocess
+import argparse
 
-pathlib.Path( pathlib.Path.cwd() / 'srcdocs' / 'clean' ).mkdir( exist_ok=True )
-src_path = pathlib.Path.cwd() / 'srcdocs'
-dest_path = src_path / 'clean'
+cwd =  pathlib.Path.cwd()
+pathlib.Path( cwd / 'srcdocs' ).mkdir( parents=True, exist_ok=True )
+src_path = cwd / 'srcdocs'
+pathlib.Path( cwd / 'build' / 'clean' ).mkdir( parents=True, exist_ok=True )
+dest_path = cwd / 'build' / 'clean'
 
 def main(argv):
-    inputfile = ''
-    outputfile = ''
-    try:
-        opts, args = getopt.getopt( argv, "hcbo:", [ "clean", "build", "output" ] )
-    except getopt.GetoptError:
-        print ( 'generate.py -c/--clean | -b/--build' )
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print ( 'generate.py -c/--clean | -b/--build' )
-            sys.exit()
-        elif opt in ( "-c", "--clean "):
-            cleanFiles()
-        elif opt in ( "-b", "--build" ):
-            build()
+    parser = argparse.ArgumentParser(description='Generate H&D Book')
+    parser.add_argument('-c','--clean', help='Cleanup input HTML files. Removes unwanted html & css. Saves files in srcdocs/clean', action='store_true' )
+    parser.add_argument('-b','--build', help='Combines the files in build/clean (created by running this script with -c option) and creates a PDF', action='store_true' )
+    parser.add_argument('-o','--output', help='Output pdf filename. Default: book.pdf', default="book.pdf" )
+    args = parser.parse_args()
 
+    if args.build:
+        build( args.output )
+    elif args.clean:
+        cleanFiles()
 
 def cleanFiles():
-	for file in sorted( src_path.rglob( '*.html' ) ):
-		with open( file, 'r' ) as src_file:
-			content = src_file.read()
-			soup = BeautifulSoup( content, 'html.parser' )
+    for file in sorted( src_path.glob( '*.html' ) ):
+        with open( file, 'r' ) as src_file:
+            content = src_file.read()
+            soup = BeautifulSoup( content, 'html.parser' )
 
-			for style in soup.head.find_all( 'style' ):
-				style.decompose()
+            for style in soup.head.find_all( 'style' ):
+                style.decompose()
 
-			for class_el in soup.select( '[class]' ):
-				del class_el[ 'class' ]
+            for class_el in soup.select( '[class],[id]' ):
+                del class_el[ 'class' ]
+                del class_el[ 'id' ]
 
-			for p_span in soup.select( 'p > span, h1 > span' ):
-				p_span.unwrap()
+            for p_span in soup.select( 'p > span, h1 > span' ):
+                p_span.unwrap()
 
-			for empty_el in soup.select( ':empty' ):
-				empty_el.decompose()
+            for empty_el in soup.select( ':empty' ):
+                empty_el.decompose()
 
-			clean = soup.prettify()
+            clean = soup.prettify()
 
-			with open( dest_path / file.name, 'w' ) as dest_file:
-				dest_file.write( clean )
+            with open( dest_path / file.name, 'w' ) as dest_file:
+                dest_file.write( clean )
+                print( "Wrote build/clean/%s " % os.path.basename( dest_file.name ) )
 
-def build():
-	template = pathlib.Path.cwd() / 'lib' / 'template.html'
-	with open( template, 'r' ) as book:
-		content = book.read()
-	book_html = BeautifulSoup( content, 'html.parser' )
-	container = book_html.body.div
 
-	for file in sorted( dest_path.rglob( '*.html' ) ):
-		with open( file, 'r' ) as src_file:
-			content = src_file.read()
-			body = formatDocument( content )
-			container.append( body )
+def build( output_filename ):
+    template = pathlib.Path.cwd() / 'lib' / 'template.html'
+    with open( template, 'r' ) as book:
+        content = book.read()
+    book_html = BeautifulSoup( content, 'html.parser' )
+    container = book_html.body.div
 
-	dest_file = pathlib.Path.cwd() / 'build' / 'book.html'
-	with open( dest_file, 'w' ) as output:
-		output.write( str(book_html) )
+    for file in sorted( dest_path.rglob( '*.html' ) ):
+        with open( file, 'r' ) as src_file:
+            content = src_file.read()
+            body = formatDocument( content )
+            container.append( body )
 
-	pdf_path = pathlib.Path.cwd() / 'build' / 'book.pdf'
-	print( str( pdf_path ) )
+    dest_file = pathlib.Path.cwd() / 'build' / 'book.html'
+    with open( dest_file, 'w' ) as output:
+        output.write( book_html.prettify() )
 
-	subprocess.Popen( [ 'weasyprint %s %s' % ( dest_file, pdf_path ) ], shell = True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL )
+    pdf_path = pathlib.Path.cwd() / 'build' / output_filename
+    print( str( pdf_path ) )
+
+    subprocess.Popen( [ 'weasyprint %s %s' % ( dest_file, pdf_path ) ], shell = True )
 
 def formatDocument( content ):
-	soup = BeautifulSoup( content, 'html.parser' )
-	body = soup.body
-	body.name = 'article'
-	body[ 'class' ] = 'cols-' + str( random.randint( 2, 4 ) ) + ' bodyfont-' + str( random.randint( 1, 8 ) )
-	for header in body.select( 'h1, h2' ):
-		header[ 'class' ] = "headingfont-" + str( random.randint( 1, 18 ) )
+    soup = BeautifulSoup( content, 'html.parser' )
+    body = soup.body
 
-	return body
+    wrapper = soup.new_tag("article")
+    body.wrap( wrapper )
 
+    body.name = 'div'
+    body[ 'class' ] = 'columns cols-' + str( random.randint( 2, 4 ) ) + ' bodyfont-' + str( random.randint( 1, 8 ) )
+    for header in body.select( 'h1, h2' ):
+        rndHeadingFont( header )
+    for h1 in body.select( 'h1' ):
+        print( h1.string )
+        wrapper.insert( 0, h1 )
+    return wrapper
+
+def rndHeadingFont( header ):
+    header[ 'class' ] = "headingfont-" + str( random.randint( 1, 18 ) )
+    return header
 
 if __name__ == "__main__":
    main( sys.argv[ 1: ] )
--- readme.md
+++ readme.md
@@ -1,9 +1,32 @@
 # H&D book 2018
 
+
+## Installation
+
 Make sure you have weasyprint installed as per instructions :
 https://weasyprint.readthedocs.io/en/stable/install.html
 
+### Mac
+On mac you'll have to install some stuff with Homebrew, so read the guide.
+We had a problem with a Cairo version, so we had to force a version with
+```pip install cairocf==0.9.0```
+
+Then:
+
 ``` pip install WeasyPrint ```
+
+And install the other dependencies
+
+``` pip install pathlib beautifulsoup4 ```
+
+We had locale errors on some machines. Set locale with:
+```
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+```
+or add those to the shells .rc file
+
+## Usage
 
 Export the documents from Google docs and place in ./srcdocs
 
@@ -18,3 +41,6 @@ Run build command
 ``` ./generate.sh -b ```
 
 That will generate ./build/book.html & run Weasyprint to generate ./build/book.pdf
+
+Set the output filename by adding
+``` ./generate.sh -b --output hdbook.pdf ```
